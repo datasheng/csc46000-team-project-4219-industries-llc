431155a5f6bf9bbb42febdd0de8c442919c56802
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-11-21 19:20:55 -0500

    removed unused dependency

--- WebScrape.py
+++ WebScrape.py
@@ -24,7 +24,6 @@ class WebScrape:
         '''
         # https://www.selenium.dev/selenium/docs/api/py/webdriver_remote/selenium.webdriver.remote.webelement.html
         from selenium import webdriver
-        from selenium.webdriver.common.keys import Keys
         from selenium.webdriver.common.by import By
         from datetime import datetime, timedelta
         from selenium.webdriver.chrome.options import Options
