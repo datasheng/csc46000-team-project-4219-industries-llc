81292ad3e7a8cb75826dc853bf0ed6eee26445e7
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2024-11-22 00:12:18 -0500

    modifications to web scrape class to prevent google from catching on

--- WebScrape.py
+++ WebScrape.py
@@ -10,8 +10,9 @@ class WebScrape:
 
     def __init__(self) -> None:
         self.emotion = Sentiment()
+        self.n = 258
 
-    def get_links(self, query: str, num_days: int) -> list[(str, str)]:
+    def get_links(self, query: str) -> list[(str, str)]:
         '''
         gets links for the specified number of days for AMD and NVIDIA
         selenium -> chrome instance 
@@ -35,9 +36,10 @@ class WebScrape:
         today = datetime.today()
         date_format = "{0}/{1}/{2}"
         url = "https://www.google.com/search?q={0}&num=10&sca_esv=21c38533450dea46&sxsrf=ADLYWIJ3l27vO1gJ2FnAVnnGDJEt0tUsvg:1731969105337&source=lnt&tbs=cdr:1,cd_min:{1},cd_max:{2}&tbm="
-        
-        for i in range(0, num_days):
+
+        for i in range(self.n, 365*2):
             day = today + timedelta(days=-i)
+            print(day)
             local_date = date_format.format(day.month, day.day, day.year)
             day_url = url.format(query, local_date, local_date)
 
@@ -45,19 +47,25 @@ class WebScrape:
             links = driver.find_elements(By.TAG_NAME, "a")
             n = len(links)
             j = 33
-            res = [] 
+            res = []
+            print(n)
+
+            if n == 3:
+                driver = webdriver.Chrome(options=chrome_options)
+                driver.get(day_url)
+                links = driver.find_elements(By.TAG_NAME, "a")
+                n = len(links)
+                j = 33
+                res = []
+                print(n)
 
             while len(res) < 2 and j < n:
-                # href = str(links[j].get_dom_attribute('href'))
                 link_text = str(links[j].text)
-
-                # if self.validate_link(href):
-                    # res.append(href)
+                print(link_text)
                 if link_text:
                     res.append(link_text) 
                 j += 1
 
-            # result.append(self.get_html(res))
             result.append(res)
             print(f"Day {i} done")
 
@@ -81,7 +89,6 @@ class WebScrape:
                 soup = BeautifulSoup(response.text, features="html.parser")
                 html_bodies.append(str(soup.get_text()))
             except requests.exceptions.RequestException as e:
-                # print(f"Error fetching URL: {link}, {e}")
                 html_bodies.append("")
         return html_bodies
     
@@ -91,13 +98,11 @@ class WebScrape:
         try:
             _ = requests.get(href, timeout=5)
         except Exception as e:
-            # print(f"Error parsing URL: {href}, {e}")
             return False
 
         conds = href is not None
         conds *= href.startswith("https://")
         conds *= "google.com" not in href
-        # print(href, conds)
         return bool(conds)
 
     def get_sentiment(self, htmls: list[str]):
@@ -106,35 +111,37 @@ class WebScrape:
         '''
         res = []
         for html in htmls:
+            print(html)
             sentiment = sum(self.emotion.get_sentiment(html)) / 2
             res.append(sentiment)
 
         return res
 
-    def save(self, arr, n):
+    def save(self, arr):
         from datetime import datetime, timedelta
         
         today = datetime.today()
         date_format = "{0}/{1}/{2}"
 
-        with open("data.json", "w") as f:
-            f.write("{\n")
-            for i in range(0, n):
+        with open("data.json", "a") as f:
+            # f.write("{\n")
+            j = 0
+            for i in range(self.n, self.n * 2):
                 if i != 0:
                     f.write(",\n")
                 day = today + timedelta(days=-i)
                 local_date = date_format.format(day.month, day.day, day.year)
         
-                f.write(f"\t\"{local_date}\": {arr[i]}")
-            f.write("\n}")        
+                f.write(f"\t\"{local_date}\": {arr[j]}")
+                j += 1
+            # f.write("\n}")        
         
 
 if __name__ == "__main__":
     web = WebScrape()
-    n = 365 * 2
     print("Getting HTML Links")
-    amd_links = web.get_links('amd+stocks', n)
+    amd_links = web.get_links('amd+stocks')
     print("Getting Sentiment")
     sentiment = web.get_sentiment(amd_links)
-    web.save(sentiment, n)
+    web.save(sentiment)
 
